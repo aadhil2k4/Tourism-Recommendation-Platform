232ff348d0d34233a72e629ef56755d4ef761dc4
Aadhil Ahamed <aadhil2k4@gmail.com>
2025-02-17 22:42:54 +0530

    implemented signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,41 @@
-import exprss from "express";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationToken.js";
 
-export const signup = async(req, res) => {
-    res.send("Signup route");
+export const signup = async (req, res) => {
+    const {email, name,password} = req.body;
+    try {
+        if(!name || !email || !password){
+            throw new Error("All fields are required");
+        }
+        const userAlreadyExists = await User.findOne({email});
+        if(userAlreadyExists){
+            throw new Error("User already exists");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const verificationToken = generateVerificationToken();
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24*60*60*1000
+        })
+        await user.save();
+
+        generateTokenAndSetCookie(res, user._id);
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            }
+        })
+    } catch (error) {
+        res.staus(400).json({success:false, message: error.message});
+    }
 }
 
 export const login = async(req, res) => {
