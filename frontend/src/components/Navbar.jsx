4e44a580af6bb42a8b6592212b01c7a3d8002679
Aadhil Ahamed <aadhil2k4@gmail.com>
2025-02-24 23:48:53 +0530

    login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40">
         <div className="container mx-auto px-4 h-16">
           <div className="flex items-center justify-between h-full">
             <div className="flex items-center gap-8">
@@ -23,17 +23,17 @@ const Navbar = () => {
             </div>
             <div className="flex items-center gap-2">
               {theme === "light" ? (
-                <button className="btn btn-sm gap-2 transition-colors border-black" onClick={() => setTheme("dark")}>
+                <button className="btn btn-sm gap-2 transition-colors" onClick={() => setTheme("halloween")}>
                   <LightbulbOff className="w-4 h-4" />
                   <span className="hidden sm:inline">DarkMode</span>
                 </button>
                 ) : (
-                  <button className="btn btn-sm gap-2 transition-colors border-black" onClick={() => setTheme("light")}>
+                  <button className="btn btn-sm gap-2 transition-colors" onClick={() => setTheme("light")}>
                     <Lightbulb className="w-4 h-4" />
                     <span className="hidden sm:inline">LightMode</span>
                   </button>
                 )}
-                <Link to={"/login"} className={`btn btn-sm gap-2 transition-colors border-black`}>
+                <Link to={"/login"} className={`btn btn-sm gap-2 transition-colors`}>
                   <LogIn className="w-4 h-4" />
                   <span className="hidden sm:inline">LogIn</span>
                 </Link>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,111 @@
+import { useState } from "react";
+import authImageLight from "../assets/authImageLight.png";
+import { Mail, Lock, EyeOff, Eye } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState();
+  const [formData, setFormData] = useState();
 
-export default LoginPage
+  return (
+    <div className="h-screen grid lg:grid-cols-2">
+      {/** Left Side Image */}
+      <div className="w-full h-screen hidden lg:block">
+        <div className="w-full h-full">
+          <img
+            src={authImageLight}
+            alt="Auth Light"
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </div>
+      {/** Right Side Form */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md spce-y-8">
+          <div className="text-center mb-8">
+            <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+            <p className="text-base-content/60">Sign in to your account</p>
+          </div>
+          {/**SignIn Form */}
+          <form className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="h-5 w-5 text-base-content/40" />
+                </div>
+                <input
+                  type="email"
+                  className="input input-bordered w-full pl-10"
+                  placeholder="you@example.com"
+                />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-5 w-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="input input-bordered w-full pl-10"
+                  placeholder="••••••••"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-5 w-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="h-5 w-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary w-full">
+              Sign in
+            </button>
+          </form>
+          <div className="flex justify-between items-center mt-4 text-base-content/60 text-sm">
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="link link-primary">
+                Create account
+              </Link>
+            </p>
+            <Link to="/forgotPassword" className="link link-primary">
+              Forgot Password
+            </Link>
+          </div>
+          
+        </div>
+        <div className="text-center my-4">
+        <p className="px-3 text-sm">OR</p>
+        <div>
+        <button className="flex items-center justify-center w-full border border-gray-300 rounded-lg py-3 px-4 font-medium mt-4">
+  <img 
+  src="https://w7.pngwing.com/pngs/882/225/png-transparent-google-logo-google-logo-google-search-icon-google-text-logo-business-thumbnail.png" 
+    alt="Google logo" 
+    className="w-5 h-5 mr-2"
+  />
+  Continue with Google
+</button>
+
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
+
